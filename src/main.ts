3b2169fa3d013a769cda2791d82c9478bb387e7d
MacBook Pro M1 512gb <bishcase@MacBook-Pro-MacBook.local>
2024-02-06 21:17:21 +0300

    123

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function start() {
+  const PORT = Number(process.env.PORT) || 9000;
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(PORT, () => {
+    console.log(`started on port: ${PORT}`);
+  });
+}
+
+start();
